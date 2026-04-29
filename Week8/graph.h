b6defbc1f5238ef52495af3fcbbc255169aeed7f
pkhanal61 <prasannaofficial61@gmail.com>
2026-04-29 22:54:47 +1000

    added graph.h

---
+++ Week8/graph.h
@@ -0,0 +1,32 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct edge {
+    int to_vertex;
+    int weight;
+} Edge;
+
+typedef struct edgeNode {
+    Edge edge;
+    struct edgeNode *next;
+} *EdgeNodePtr;
+
+typedef struct edgeList {
+    EdgeNodePtr head;
+} EdgeList;
+
+typedef struct graph {
+    int V;
+    EdgeList *edges;
+} Graph;
+
+// function prototypes 
+void add_edge(Graph *self, int from, int to, int w);
+Graph load_graph(const char *filename);
+void print_indegrees(Graph *G);
+void free_graph(Graph *G);
+
+#endif
