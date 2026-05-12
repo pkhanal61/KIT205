6a6a876d0e5d5dacf5197243314ec78c611b4ec8
pkhanal61 <prasannaofficial61@gmail.com>
2026-05-12 20:39:27 +1000

    Defined artistName and updated function prototypes

--- Assignment_1/bst.h
+++ Assignment_1/bst.h
@@ -4,14 +4,15 @@
 typedef struct SongNode {
 
     char title[100];
+    char artistName[100]; // it stores artist
 
     struct SongNode* left;
     struct SongNode* right;
 
 } SongNode;
 
-SongNode* create_song_node(char title[]);
-SongNode* insert_song(SongNode* root, char title[]);
+SongNode* create_song_node(char title[], char artistName[]);
+SongNode* insert_song(SongNode* root, char title[], char artistName[]);
 void inorder_songs(SongNode* root);
 
 #endif
