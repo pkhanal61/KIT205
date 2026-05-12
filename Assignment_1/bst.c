4f21c5e7e157daec597685bc9de015d6e6fdf4f1
pkhanal61 <prasannaofficial61@gmail.com>
2026-05-12 20:34:54 +1000

    Added artistName

--- Assignment_1/bst.c
+++ Assignment_1/bst.c
@@ -4,11 +4,12 @@
 
 #include "bst.h"
 
-SongNode* create_song_node(char title[]) {
+SongNode* create_song_node(char title[], char artistName[]) {
 
-    SongNode* newNode = (SongNode*)malloc(sizeof(SongNode)); //allocating memory
+    SongNode* newNode = (SongNode*)malloc(sizeof(SongNode));
 
-    strcpy(newNode->title, title); //it stores the song title
+    strcpy(newNode->title, title); //it store songs
+    strcpy(newNode->artistName, artistName); //it store artists
 
     newNode->left = NULL;
     newNode->right = NULL;
@@ -16,20 +17,17 @@ SongNode* create_song_node(char title[]) {
     return newNode;
 }
 
-SongNode* insert_song(SongNode* root, char title[]) {
+SongNode* insert_song(SongNode* root, char title[], char artistName[]) {
 
     if (root == NULL) {
-        return create_song_node(title); 
+        return create_song_node(title, artistName);
     }
 
     if (strcmp(title, root->title) < 0) {
-
-        root->left = insert_song(root->left, title); //going left
-
+        root->left = insert_song(root->left, title, artistName);
     }
     else if (strcmp(title, root->title) > 0) {
-
-        root->right = insert_song(root->right, title); //going right
+        root->right = insert_song(root->right, title, artistName);
     }
 
     return root;
@@ -37,13 +35,11 @@ SongNode* insert_song(SongNode* root, char title[]) {
 
 void inorder_songs(SongNode* root) {
 
-    if (root == NULL) {
-        return;
-    }
+    if (root == NULL) return;
 
     inorder_songs(root->left);
 
-    printf("%s\n", root->title); //it prints the current node
+    printf("%s - %s\n", root->title, root->artistName);
 
     inorder_songs(root->right);
 }
