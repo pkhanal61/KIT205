104931b03a713bd51174c0f3e6752df54bab3e29
pkhanal61 <prasannaofficial61@gmail.com>
2026-05-14 18:54:30 +1000

    Added BST node structure for many to many

--- Assignment_1/bst.h
+++ Assignment_1/bst.h
@@ -1,18 +1,24 @@
 #ifndef BST_H
 #define BST_H
 
-typedef struct SongNode {
+// linked list to store artist names for each song
+typedef struct ArtistListBST {
+    char name[100];
+    struct ArtistListBST* next;
+} ArtistListBST;
 
+//BST node
+typedef struct SongNode {
     char title[100];
-    char artistName[100]; // it stores artist
+    ArtistListBST* artists;
 
     struct SongNode* left;
     struct SongNode* right;
 
 } SongNode;
 
-SongNode* create_song_node(char title[], char artistName[]);
-SongNode* insert_song(SongNode* root, char title[], char artistName[]);
-void inorder_songs(SongNode* root);
+//Functions
+SongNode* create_song_node(char title[]);
+SongNode* insert_song(SongNode* root, char title[]);
 
 #endif
