d927fda0ab33d2a7b8bff4632aa2514d34d02ae7
pkhanal61 <prasannaofficial61@gmail.com>
2026-05-16 21:53:01 +1000

    Updated performance evaluation for both prototypes across different data sizes

--- Assignment_1/evaluation.c
+++ Assignment_1/evaluation.c
@@ -7,58 +7,58 @@
 
 void run_evaluation() {
 
-    Proto1DB db1;
-    Proto2DB db2;
+    // test with different sizes to see how performance scales
+    int sizes[] = {1000, 2000, 3000, 4000, 5000};
+    int num_sizes = 5;
 
-    init_db(&db1);
-    init_proto2(&db2);
+    printf("\nPERFORMANCE RESULTS:\n");
+    printf("%-10s %-20s %-20s\n", "Size", "Prototype1 (s)", "Prototype2 (s)");
+    printf("------------------------------------------------------\n");
 
-    clock_t start, end;
+    for (int s = 0; s < num_sizes; s++) {
 
-    //Prototype 1
+        int n = sizes[s];
 
-    start = clock();
+        // fresh database for each test
+        Proto1DB db1;
+        Proto2DB db2;
 
-    for (int i = 0; i < 5000; i++) {
+        init_db(&db1);
+        init_proto2(&db2);
 
-        char artist[50];
-        char song[50];
+        clock_t start, end;
 
-        sprintf(artist, "Artist%d", i);
-        sprintf(song, "Song%d", i);
+// For prototype 1
+        start = clock();
 
-        add_artist(&db1, artist);
-        add_song_to_artist(&db1, artist, song);
+        for (int i = 0; i < n; i++) {
+            char artist[50];
+            char song[50];
+            sprintf(artist, "Artist%d", i);
+            sprintf(song, "Song%d", i);
+            add_artist(&db1, artist);
+            add_song_to_artist(&db1, artist, song);
+        }
+
+        end = clock();
+        double proto1_time = (double)(end - start) / CLOCKS_PER_SEC;
+
+// For prototype 2
+        start = clock();
+
+        for (int i = 0; i < n; i++) {
+            char artist[50];
+            char song[50];
+            sprintf(artist, "Artist%d", i);
+            sprintf(song, "Song%d", i);
+            add_artist_p2(&db2, artist);
+            add_song_p2(&db2, artist, song);
+        }
+
+        end = clock();
+        double proto2_time = (double)(end - start) / CLOCKS_PER_SEC;
+
+// print result for this size
+        printf("%-10d %-20f %-20f\n", n, proto1_time, proto2_time);
     }
-
-    end = clock();
-
-    double proto1_time = (double)(end - start) / CLOCKS_PER_SEC;
-
-    //Prototype 2
-
-    start = clock();
-
-    for (int i = 0; i < 5000; i++) {
-
-        char artist[50];
-        char song[50];
-
-        sprintf(artist, "Artist%d", i);
-        sprintf(song, "Song%d", i);
-
-        add_artist_p2(&db2, artist);
-        add_song_p2(&db2, artist, song);
-    }
-
-    end = clock();
-
-    double proto2_time = (double)(end - start) / CLOCKS_PER_SEC;
-
-    // results
-    printf("\nPERFORMANCE RESULTS : \n");
-
-    printf("Prototype 1 Time: %f seconds\n", proto1_time);
-
-    printf("Prototype 2 Time: %f seconds\n", proto2_time);
 }
